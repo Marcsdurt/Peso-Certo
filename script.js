005b772de68efe95475c988dc9d003062e25d512
Marcos Duarte <106616718+Marcsdurt@users.noreply.github.com>
2023-11-29 20:11:45 -0300

    Adicionei as categorias típicas de IMC

--- script.js
+++ script.js
@@ -7,7 +7,15 @@ form.addEventListener("submit", function(event) {
     const alturaInput = document.getElementById('altura').value;
     const imc = (pesoInput / (alturaInput * alturaInput)).toFixed(2);
     const valorSpan = document.querySelector("#valor");
-    valorSpan.textContent = `Seu IMC é: ${imc}`;
 
-}
-);
+    if (imc < 18.5) {
+        valorSpan.textContent = `Seu IMC é: ${imc}  Subpeso`;
+    } else if (imc >= 18.5 && imc <= 24.9) {
+        valorSpan.textContent = `Seu IMC é: ${imc}  Peso normal`;
+    } else if (imc >= 25 && imc <= 29.9) {
+        valorSpan.textContent = `Seu IMC é: ${imc}  Sobrepeso`;
+    } else {
+        valorSpan.textContent = `Seu IMC é: ${imc}  Obesidade`;
+    }
+
+});
